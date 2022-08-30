c7ac29cfc1c1420e8731e864aec09d7f12ff24e9
Jamil <jamil_salim_daruix@hotmail.com>
2022-08-29 22:03:23 -0300

    commit inicial

---
+++ projeto-pizza/projeto-pizza-api/Controllers/ExemplosController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace projeto_pizza_api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ExemplosController : ControllerBase
+    {
+        private static List<PizzaDto> minhaLista = new();
+
+        [HttpGet]
+        public IEnumerable<PizzaDto> Get()
+        {
+            return minhaLista; 
+        }
+
+        [HttpPost("post/{codigo}/{nome}")]
+        public PizzaDto Post([FromRoute] int codigo, [FromRoute] string nome) 
+        {
+            var pizza = new PizzaDto(codigo) { Nome = nome };
+            minhaLista.Add(pizza);
+            return pizza ;
+        }
+
+        [HttpPut("put/{nome}")]
+        public PizzaDto Put([FromQuery] int codigo, [FromRoute] string nome)
+        {
+            var pizza = minhaLista.Where(w => w.Codigo == codigo).FirstOrDefault();
+            pizza!.Nome = nome;
+            return pizza;
+        }
+    }
+
+    public record PizzaDto(int Codigo)
+    {
+        public string Nome { get; set; } = "";
+        public bool Validar(string parametro) => Nome == parametro;
+    }
+}
